17e12499cec404df7ae97c3a1776772f66484760
Elio Gubser <gubser@users.noreply.github.com>
2015-06-29 17:56:40 +0200

    project refactoring, creating config, construction area everywhere

--- pathspider/__init__.py
+++ pathspider/__init__.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import json
 
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'VERSION')) as version_file:
@@ -7,7 +8,16 @@ with open(os.path.join(here, 'VERSION')) as version_file:
 
 def main():
     # setup command line arguments
-    print("pathspider {}".format(version))
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--version', action='version', version='%(prog)s '+version)
+    parser.add_argument('--mode', '-m', choices=['standalone', 'client', 'service'], required=True, help='Set the operating mode.')
+    parser.add_argument('--config', '-c', default='pathspider.cfg', help='Set pathspider configuration')
+
+    args = parser.parse_args()
+    cfg = json.load(open(os.path.join(here, 'pathspider.cfg'), 'rt'))
+
+
 
 if __name__ == '__main__':
     main()
+
---
+++ pathspider/webresolver/__init__.py
@@ -0,0 +1,87 @@
+"""
+webresolver: Resolve a large number of domains to IPv4 and IPv6 addresses.
+Derived from ECN Spider (c) 2014 Damiano Boppart <hat.guy.repo@gmail.com>
+
+This module implements the mPlane component module interface for webresolver.
+
+    Copyright 2015 Elio Gubser <elio.gubser@alumni.ethz.ch>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+"""
+
+from ipaddress import ip_address
+
+import mplane
+
+import os.path
+from datetime import datetime
+import threading
+import ipfix
+
+ipfix.ie.use_iana_default()
+ipfix.ie.use_5103_default()
+scriptdir = os.path.dirname(os.path.abspath(__file__))
+ipfix.ie.use_specfile(os.path.join(scriptdir, "qof.iespec"))
+
+def services(ip4addr = None, ip6addr = None):
+    """
+    Return a list of mplane.scheduler.Service instances implementing
+    the mPlane capabilities for webresolver.
+
+    """
+
+    servicelist = []
+
+    servicelist.append(WebresolverService(webresolver_cap(ip_address(ip4addr or '0.0.0.0'), reguri)))
+
+    servicelist.append(WebresolverService(webresolver_cap(ip_address(ip6addr or '::'), reguri)))
+
+    return servicelist
+
+
+def webresolver_cap(ipaddr, reguri):
+    ipv = "ip"+str(ipaddr.version)
+
+    cap = mplane.model.Capability(label='webresolver-'+ipv, when='now ... future', reguri=reguri)
+
+    cap.add_metadata("source."+ipv, ipaddr)
+    cap.add_metadata("source.port", port)
+
+    cap.add_parameter("btdhtspider.count")
+    cap.add_parameter("btdhtspider.unique")
+
+    cap.add_result_column("destination."+ipv)
+    cap.add_result_column("destination.port")
+    cap.add_result_column("btdhtspider.nodeid")
+
+    return cap
+
+class WebresolverService(mplane.scheduler.Service):
+    def __init__(self, cap):
+        super().__init__(cap)
+
+    def run(self, spec, check_interrupt):
+
+        starttime = datetime.utcnow()
+
+        res = mplane.model.Result(specification=spec)
+        stoptime = datetime.utcnow()
+
+        res.set_when(mplane.model.When(a=starttime, b=stoptime))
+
+        return res
+
+
