6333167b046d40a9ebe6813d2ec963012df4d14a
Elio Gubser <gubser@users.noreply.github.com>
2015-10-19 22:22:13 +0200

    fixed project dependencies

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['ipfix', 'bencodepy', 'numpy', 'pandas', 'py3dns'],
+    install_requires=['ipfix', 'bencodepy', 'numpy', 'pandas', 'dnspython3'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
