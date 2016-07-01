ffdf7caa618963fd8a35e483f85bb9f0fed8740c
Iain R. Learmonth <irl@debian.org>
2016-07-01 14:45:27 +0100

    Fixes for setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,3 @@
-"""A setuptools based setup module.
-
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
-"""
-
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 # To use a consistent encoding
@@ -32,7 +25,7 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/mami-project/pathspider',
+    url='https://pathspider.mami-project.eu/',
 
     # Author details
     author='Elio Gubser',
@@ -63,7 +56,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    packages=find_packages(exclude=['contrib', 'doc', 'tests*']),
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
