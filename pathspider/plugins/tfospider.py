a585b0ac9b275b07fecb7b45e7658cd80032704a
Brian Trammell <brian@trammell.ch>
2016-10-04 13:23:31 +0200

    update TFO spider: simplified options parsing, more detailed output

--- pathspider/plugins/tfospider.py
+++ pathspider/plugins/tfospider.py
@@ -15,7 +15,7 @@ from pathspider.observer import basic_flow
 from pathspider.observer import basic_count
 
 Connection = collections.namedtuple("Connection", ["client", "port", "state"])
-SpiderRecord = collections.namedtuple("SpiderRecord", ["ip", "rport", "port",
+TFOSpiderRecord = collections.namedtuple("TFOSpiderRecord", ["ip", "rport", "port",
                                                        "host", "tfostate",
                                                        "connstate", "rank"])
 
@@ -27,116 +27,111 @@ USER_AGENT = "pathspider"
 
 ## Chain functions
 
-def tcpcompleted(rec, tcp, rev): # pylint: disable=W0612,W0613
-    return not tcp.fin_flag
+TO_EOL = 0
+TO_NOP = 1
+TO_MSS = 2
+TO_WS = 3
+TO_SACKOK = 4
+TO_SACK = 5
+TO_TS = 8
+TO_MPTCP = 30
+TO_FASTOPEN = 34
+TO_EXPA = 254
+TO_EXPB = 255
+TO_EXP_FASTOPEN = (0xF9, 0x89)
 
-def tfosetup(rec, ip):
-    rec['tfo_seq'] = -1000
-    rec['tfo_len'] = -1000
-    rec['tfoworking'] = 0
-    return True
-
-def _tfocookie(tcp):
+def _tcpoptions(tcp):
     """
-    Determine whether a TCP header (from python-libtrace) contains a TFO cookie or not.
+    Given a TCP header, make TCP options available
+    according to the interface we've designed for python-libtrace
+    
     """
-
-    options = tcp.data[20:tcp.doff*4]
+    optbytes = tcp.data[20:tcp.doff*4]
+    opthash = {}
+    
+    # shortcut empty options
+    if len(optbytes) == 0:
+        return opthash
+    
+    # parse options in place
     cp = 0
-
-    while True:
-        if not options:
-            return None
-        if (options[cp] == 0):
-            return None
-        if options[cp] == 1:
+    ncp = 0
+    
+    while cp < len(optbytes):
+        # skip NOP
+        if optbytes[cp] == TO_NOP:
             cp += 1
             continue
-        if options[cp] == 34:
-            # IANA-allocated option
-            if options[cp+1] == 2:
-                cp += 2
-                continue
-            else:
-                return (0,34)
-        if options[cp] == 253:
-            # Experimental option 253
-            if (options[cp+2], options[cp+3]) == (249, 137):
-                if options[cp+1] <= 4:
-                    cp += options[cp+1]
-                    continue
-                else:
-                    return (0,253)
-        if options[0] == 254:
-            # Experimental option 254
-            if (options[cp+2], options[cp+3]) == (249, 137):
-                if options[cp+1] <= 4:
-                    options = options[cp+1]
-                    continue
-                else:
-                    return (0,254)
-        else:
-            if options[cp+1] <= len(options):
-                return False
-            else:
-                cp += options[cp+1]
-                continue
-    return False
-
-def tfoworking(rec, tcp, rev):
+        # die on EOL
+        if optbytes[cp] == TO_EOL:
+            break 
+        
+        # parse options length
+        ncp = cp + optbytes[cp+1]
+        
+        # copy options data into hash
+        # FIXME doesn't handle multiples
+        opthash[optbytes[cp]] = optbytes[cp+2:ncp]
+        
+        # advance
+        cp = ncp
     
-    outgoing = (rec['sp'] == tcp.src_port)
-    data_len = (len(tcp.data) - tcp.doff*4)
-    has_cookie = bool(_tfocookie(tcp))
-    has_data = (data_len > 0)
+    return opthash
+
+def _tfocookie(tcp):
+    opts = _tcpoptions(tcp)
     
-    if ((rec['tfo_seq'] < 0) & (not has_cookie)):#no TFO data sent or received
-        return True
+    if TO_FASTOPEN in opts:
+        return (TO_FASTOPEN, bytes(opts[TO_FASTOPEN]))
+    elif TO_EXPA in opts and opts[TO_EXPA][0:2] == bytearray(TO_EXP_FASTOPEN):
+        return (TO_EXPA, bytes(opts[TO_EXPA][2:]))
+    elif TO_EXPB in opts and opts[TO_EXPB][0:2] == bytearray(TO_EXP_FASTOPEN):
+        return (TO_EXPB, tuple(opts[TO_EXPA][2:]))
+    else:
+        return (None, None)
+
+def _tfopacket(rec, tcp, rev):
+    # Check for TFO cookie and data on SYN
+    if tcp.syn_flag and not tcp.ack_flag:
+        (tfo_kind, tfo_cookie) = _tfocookie(tcp)
+        if tfo_kind is not None:
+            rec['tfo_kind'] = tfo_kind
+            rec['tfo_cklen'] = len(tfo_cookie)
+            rec['tfo_seq'] = tcp.seq_nbr
+            rec['tfo_len'] = len(tcp.data) - tcp.doff*4
+            rec['tfo_ack'] = 0
     
-    if (has_cookie & has_data & outgoing):#TFO data sent
-        rec['tfo_seq'] = tcp.seq_nbr
-        rec['tfo_len'] = data_len
-        rec['tfoworking'] = 1
-        return True
-       
-    if ((not outgoing) & (rec['tfo_seq'] > 0) & (tcp.ack_nbr == rec['tfo_seq'] + rec['tfo_len'] + 1)):#TFO acknowledged
-        rec['tfoworking'] = 2
-        return False
-    
-    if (outgoing & has_data & (tcp.seq_nbr == rec['tfo_seq'] + 1)):#TCP fall back, retransmission
-        rec['tfo_seq'] = -500
-        rec['tfo_len'] = -500
-        return True
-    
-    return True
+    # Look for ACK of TFO data
+    elif tcp.syn_flag and tcp.ack_flag and 'tfo_kind' in rec:
+        rec['tfo_ack'] = tcp.ack_nbr    
 
-def test_tfocookie(fn=_tfocookie):
-    """
-    Test the _tfocookie() options parser on a static packet dump test file.
-    This is used mainly for performance evaluation of the parser for now,
-    and does not check for correctness.
+# def test_tfocookie(fn=_tfocookie):
+#     """
+#     Test the _tfocookie() options parser on a static packet dump test file.
+#     This is used mainly for performance evaluation of the parser for now,
+#     and does not check for correctness.
 
-    """
-    import plt as libtrace
+#     """
+#     import plt as libtrace
 
-    lturi = "pcapfile:testdata/tfocookie.pcap"
-    trace = libtrace.trace(lturi)
-    trace.start()
-    pkt = libtrace.packet()
-    cookies = 0
-    nocookies = 0
+#     lturi = "pcapfile:testdata/tfocookie.pcap"
+#     trace = libtrace.trace(lturi)
+#     trace.start()
+#     pkt = libtrace.packet()
+#     cookies = 0
+#     nocookies = 0
 
-    while trace.read_packet(pkt):
-        if not pkt.tcp:
-            continue
+#     while trace.read_packet(pkt):
+#         if not pkt.tcp:
+#             continue
 
-        # just do the parse
-        if fn(pkt.tcp):
-            cookies += 1
-        else:
-            nocookies += 1
+#         # just do the parse
+#         if fn(pkt.tcp):
+#             cookies += 1
+#         else:
+#             nocookies += 1
 
-    print("cookies: %u, nocookies: %u" % (cookies, nocookies))
+#     print("cookies: %u, nocookies: %u" % (cookies, nocookies))
 
 
 ## TFOSpider main class
@@ -201,9 +196,9 @@ class TFOSpider(Spider):
 
     def post_connect(self, job, conn, pcs, config):
         if conn.state == CONN_OK:
-            rec = SpiderRecord(job[0], job[1], conn.port, job[2], config, True, job[3])
+            rec = TFOSpiderRecord(job[0], job[1], conn.port, job[2], config, True, job[3])
         else:
-            rec = SpiderRecord(job[0], job[1], conn.port, job[2], config, False, job[3])
+            rec = TFOSpiderRecord(job[0], job[1], conn.port, job[2], config, False, job[3])
 
         try:
             conn.client.shutdown(socket.SHUT_RDWR)
@@ -225,7 +220,7 @@ class TFOSpider(Spider):
                             new_flow_chain=[basic_flow, tfosetup],
                             ip4_chain=[basic_count],
                             ip6_chain=[basic_count],
-                            tcp_chain=[tfoworking, tcpcompleted])
+                            tcp_chain=[_tfopacket])
         except:
             logger.error("Observer not cooperating, abandon ship")
             traceback.print_exc()
