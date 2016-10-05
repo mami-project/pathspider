45904db4bb63d5e46d53e08fbe8f509e79d7382b
Iain R. Learmonth <irl@fsfe.org>
2016-10-05 05:09:54 +0100

    ALPN plugin works; not sure what else broke though

--- pathspider/plugins/tls.py
+++ pathspider/plugins/tls.py
@@ -13,23 +13,26 @@ from pathspider.observer import basic_count
 
 SpiderRecord = collections.namedtuple("SpiderRecord", ["ip", "rport", "port",
                                                        "host", "config",
-                                                       "connstate"])
+                                                       "connstate", "alpn"])
 
 test_ssl = ('timeout {timeout} openssl s_client '
-            '-servername {hostname} -connect {ip}:{port}  2>&1 | '
+            '-servername {hostname} -connect {ip}:{port} 2>&1 | '
             'awk \'{{ if($1 == "Server" && $2 == "public") '
-            '{{ print "GOT-SSL"; }} '
+            '{{ print "GOT-TLS"; }} '
             'if($2=="Connection" && $3 == "refused") print "NO-TLS"; '
             'if($1=="gethostbyname" || $0=="connect: No route to host") '
             'print "DNS-FAILURE"}}\'')
 
 test_alpn = ('timeout {timeout} openssl s_client '
-             '-alpn \'h2,http/1.1\' -servername {hostname} -connect {ip}:{port}  2>&1 | '
+             '-alpn \'h2,http/1.1\' -servername {hostname} -connect {ip}:{port} 2>&1 | '
              'awk \'{{if($1 == "ALPN") {{split($0, arr, ":"); '
              'print "ALPN:"arr[2];}} if($2 == "ALPN") print "NO-ALPN"; '
              'if($2=="Connection" && $3 == "refused") print "NO-TLS"; '
              'if($1=="gethostbyname" || $0=="connect: No route to host") '
              'print "DNS-FAILURE"}}\'')
+
+test_npn = ('timeout {timeout} openssl s_client '
+            '-nextprotoneg \'\' -servername {hostname} -connect {ip}:{port} 2>&1')
 
 class TLS(Spider):
     """
@@ -43,28 +46,39 @@ class TLS(Spider):
         pass
 
     def connect(self, job, pcs, config):
-        pass
-
-    def post_connect(self, job, conn, pcs, config):
         job_args = {'hostname': job[2],
                     'ip': job[0],
                     'port': job[1],
                     'timeout': self.args.timeout,
                    }
 
-        if config == 0:
-            logging.warning(subprocess.check_output(test_ssl.format(**job_args), shell=True))
-            rec = SpiderRecord(job[0], job[1], 0, job[2], config, True)
-        if config == 1:
-            logging.warning(subprocess.check_output(test_alpn.format(**job_args), shell=True))
-            rec = SpiderRecord(job[0], job[1], 0, job[2], config, True)
+        connstate = False
+        alpn = None
 
+        if config == 0:
+            ssl_status = subprocess.check_output(test_ssl.format(**job_args), shell=True).decode('ascii').strip()
+            if ssl_status == 'GOT_TLS':
+                connstate = True
+        if config == 1:
+            alpn_status = subprocess.check_output(test_alpn.format(**job_args), shell=True).decode('ascii').strip()
+            if "ALPN" in alpn_status:
+                connstate = True
+                if ":" in alpn_status:
+                    alpn = alpn_status[6:]
+
+        rec = SpiderRecord(job[0], job[1], config, job[2], config, connstate, alpn)
         return rec
+
+    def post_connect(self, job, conn, pcs, config):
+        return conn
 
     def create_observer(self):
         try:
             # this is useless
-            return Observer(self.libtrace_uri)
+            return Observer("int:lo",
+                            new_flow_chain=[basic_flow],
+                            ip4_chain=[basic_count],
+                            ip6_chain=[basic_count])
         except:
             print("Observer would not start")
             sys.exit(-1)
@@ -74,15 +88,18 @@ class TLS(Spider):
             flow = {"dip": res.ip,
                     "sp": res.port,
                     "dp": res.rport,
-                    "observed": False}
+                    "observed": False,
+                    "connstate": res.connstate,
+                    "config": res.config,
+                    "alpn": res.alpn}
         else:
             flow['observed'] = True
-
         self.outqueue.put(flow)
 
     @staticmethod
     def register_args(subparsers):
         parser = subparsers.add_parser('tls', help="Transport Layer Security")
         parser.set_defaults(spider=TLS)
-        parser.add_argument("--timeout", default=5, type=int)
+        parser.add_argument("--timeout", default=5, type=int, help="The timeout to use for attempted connections in seconds (Default: 5)")
+        parser.add_argument("--test", choices=['alpn', 'npn'], default='alpn', help="Choose to test either ALPN or NPN (Default: ALPN)")
 
