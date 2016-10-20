4ba981f74c838fb94b2ff304045d3ff577d8f6fd
Iain R. Learmonth <irl@fsfe.org>
2016-10-21 00:51:23 +0100

    Add test to ensure that a bad port doesn't make things crash

--- tests/test_job_feeder.py
+++ tests/test_job_feeder.py
@@ -40,3 +40,16 @@ def test_job_feeder_webtest_newline():
     assert spider.was_shutdown
     assert spider.jobs == expected_jobs
 
+def test_job_feeder_webtest_badport():
+    spider = FakeSpider()
+
+    expected_jobs = [['160.85.31.173', 80, 'mami-project.eu', '1'],
+                     ['139.133.210.32', 80, 'erg.abdn.ac.uk', '2'],
+                     ['2001:630:241:210:569f:35ff:fe0a:116a', 80, 'erg.abdn.ac.uk', '3'],
+                     ['2001:67c:10ec:36c2::61', 80, 'ecn.ethz.ch', '5'],
+                     ['139.133.1.4', 80, 'abdn.ac.uk', '6']]
+
+    job_feeder("tests/testdata/webtest_badport.csv", spider)
+    assert spider.was_shutdown
+    assert spider.jobs == expected_jobs
+
