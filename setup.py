86ad26edf01c2ef0e4f81c4c65ce4a930e3ddc7e
Iain R. Learmonth <irl@fsfe.org>
2016-10-21 22:03:21 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the relevant file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 with open(path.join(here, 'pathspider', 'VERSION'), encoding='utf-8') as version_file:
@@ -18,23 +18,12 @@ with open('requirements.txt') as f:
 
 setup(
     name='pathspider',
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
     version=version,
-
-    description='Tools for measuring path transparency in the Internet.',
+    description='A tool for measuring path transparency in the Internet',
     long_description=long_description,
-
-    # The project's main homepage.
-    url='https://pathspider.mami-project.eu/',
-
-    # Author details
+    url='https://pathspider.net/',
     author='Iain Learmonth',
     author_email='irl@fsfe.org',
-
-    # Choose your license
     license='GNU GPLv2',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
@@ -50,40 +39,15 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
 
-    # What does your project relate to?
-    keywords='internet measurement ecn torrent bittorrent web www analysis router congestion path transparency',
+    keywords='internet measurement ecn tfo dns web www analysis router congestion path transparency latency diffserv dscp',
 
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
     packages=find_packages(exclude=['contrib', 'doc', 'tests*']),
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
     install_requires=install_requires,
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    #extras_require={
-    #},
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    #package_data={
-    #    'sample': ['package_data.dat'],
-    #},
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
             'pathspider=pathspider.run:handle_args_wrapper',
