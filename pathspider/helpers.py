2c51a7d88b87a00ecdba90668b3b47aa21fe8af1
Piet De Vaere <piet@devae.re>
2016-11-12 10:04:38 +0100

    moved things to global variables

--- pathspider/helpers.py
+++ pathspider/helpers.py
@@ -3,33 +3,32 @@
 import socket
 import logging
 
-HTTP_REQUEST = \
-'GET / HTTP/1.1\r\n' \
-'Host: {hostname}\r\n' \
-'Connection: keep-alive\r\n' \
-'User-Agent: Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36\r\n' \
-'Accept-Encoding: gzip, deflate, sdch6\r\n' \
-'\r\n' \
-'\r\n' \
-
-HTTP_REQUEST = \
-'GET / HTTP/1.1\r\n' \
-'Host: {hostname}\r\n' \
-'User-Agent: curl/7.50.1\r\n' \
-'Acccept: */*\r\n' \
-'Connection: keep-alive\r\n' \
-'\r\n' \
-
-HTTP_REQUEST = \
-'GET / HTTP/1.1\r\n' \
-'Host: {hostname}\r\n' \
-'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0\r\n' \
-'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n' \
-'Accept-Language: en-US,en;q=0.5\r\n' \
-'Accept-Encoding: gzip, deflate\r\n' \
-'Connection: keep-alive\r\n' \
-'Cache-Control: max-age=0\r\n' \
-'\r\n\r\n' \
+HTTP_SOCKET_TIMEOUT = 10
+HTTP_NEWLINE = '\r\n'
+HTTP_USER_AGENT = 'firefox'
+HTTP_REQUEST = {
+    'curl' :    ['GET / HTTP/1.1',
+                'Host: {hostname}',
+                'User-Agent: curl/7.50.1',
+                'Acccept: */*',
+                'Connection: keep-alive',
+                '',
+                ''
+                ],
+    'firefox' : ['GET / HTTP/1.1',
+                'Host: {hostname}',
+                'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:45.0)'\
+                'Gecko/20100101 Firefox/45.0',
+                'Accept: text/html,application/xhtml+xml,application/xml;'\
+                'q=0.9,*/*;q=0.8',
+                'Accept-Language: en-US,en;q=0.5',
+                'Accept-Encoding: gzip, deflate',
+                'Connection: keep-alive',
+                'Cache-Control: max-age=0',
+                '',
+                ''
+                ]
+}
 
 def _detect_end_of_header(response):
     if response.endswith('\n'*2): return True
@@ -46,10 +45,11 @@ def _get_content_length(header):
 
     return 0
 
-def http_get(sock, host):
-    sock.settimeout(10)
+def http_get(sock, host, user_agent=HTTP_USER_AGENT):
+    sock.settimeout(HTTP_SOCKET_TIMEOUT)
     logger = logging.getLogger('http_get')
-    request = HTTP_REQUEST.format(hostname = host)
+    request = HTTP_NEWLINE.join(HTTP_REQUEST[user_agent])
+    request = request.format(hostname = host)
 
     try:
         sock.send(bytes(request, 'ASCII'))
