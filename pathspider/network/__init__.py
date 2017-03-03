550059328c9d1a3de89f631eabdb3a74f768e052
ana-cc <ana@netstat.org.uk>
2017-03-03 22:33:07 +0000

    Adds pyroute2 helper code to get addresses for interfaces (Closes: #73)

--- pathspider/network/__init__.py
+++ pathspider/network/__init__.py
@@ -7,3 +7,17 @@ def interface_up(int):
             if interface.ifname == int and interface.operstate == 'UP':
                 return True
         return False
+    
+def ipv4_address(int):
+    with IPDB() as ipdb:
+        addrset = [x[0] for x in ipdb.interfaces[int].ipaddr if '.' in x[0]]
+        if len(addrset) > 0:
+            # Should return the first IPv4 address of the interface...if there are any
+            return addrset[0]
+
+def ipv6_address(int):
+    with IPDB() as ipdb:
+        addrset = [x[0] for x in ipdb.interfaces[int].ipaddr if ':' in x[0] and not x[0].startswith('fe')]
+        if len(addrset) > 0:
+            # Should return the first IPv6 address of the interface...if there are any
+            return addrset[0]
