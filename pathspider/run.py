a0dd0142c5811ac377770536e8f0471de05c8d57
Iain R. Learmonth <irl@fsfe.org>
2017-03-08 23:44:03 +0000

    plugin_load: Allows for new 'torexits' plugins

--- pathspider/run.py
+++ pathspider/run.py
@@ -8,9 +8,16 @@ from straight.plugin import load
 from pathspider.base import PluggableSpider
 
 import pathspider.util.dnsresolv
+import pathspider.util.torrelays
 
+utils = []
 
-plugins = load("pathspider.plugins", subclasses=PluggableSpider)
+if "--tor-exits" in sys.argv:
+    plugins = load("pathspider.plugins.torexits", subclasses=PluggableSpider)
+else:
+    plugins = load("pathspider.plugins", subclasses=PluggableSpider)
+    utils.append(pathspider.util.dnsresolv)
+    utils.append(pathspider.util.torrelays)
 
 def handle_args(argv):
     class SubcommandHelpFormatter(argparse.RawDescriptionHelpFormatter):
@@ -21,31 +28,30 @@ def handle_args(argv):
                 parts += "\n\nSpider safely!"
             return parts
 
-    parser = argparse.ArgumentParser(description=('Pathspider will spider the '
+    parser = argparse.ArgumentParser(description=('PATHspider will spider the '
             'paths.'), formatter_class=SubcommandHelpFormatter)
-    parser.add_argument('-s', '--standalone', action='store_true', help='''run in
-        standalone mode. this is the default mode (and currently the only supported
-        mode). in the future, mplane will be supported as a mode of operation.''', default=True)
-    parser.add_argument('-i', '--interface', help='''the interface to use for the observer''', default="eth0")
-    parser.add_argument('-w', '--workers', type=int, help='''number of workers to use''', default=100)
-    parser.add_argument('--input', default='/dev/stdin', metavar='INPUTFILE', help='''a file
-            containing a list of remote hosts to test, with any accompanying
-            metadata expected by the pathspider test. this file should be formatted
-            as a comma-seperated values file. Defaults to standard input.''')
+    parser.add_argument('-s', '--standalone', action='store_true', help='''Run in
+        standalone mode.''', default=True)
+    parser.add_argument('-i', '--interface', help='''The interface to use for the observer.''', default="eth0")
+    parser.add_argument('-w', '--workers', type=int, help='''Number of workers to use.''', default=100)
+    parser.add_argument('--input', default='/dev/stdin', metavar='INPUTFILE', help='''A file
+            containing a list of PATHspider jobs. Defaults to standard input.''')
     parser.add_argument('--output', default='/dev/stdout', metavar='OUTPUTFILE', 
-            help='''the file to output results data to. Defaults to standard output.''')
-    parser.add_argument('-v', '--verbose', action='store_true', help='''log debug-level output.''')
+            help='''The file to output results data to. Defaults to standard output.''')
+    parser.add_argument('--tor-exits', action='store_true', help='''Tor exit plugins (server mode).''')
+    parser.add_argument('-v', '--verbose', action='store_true', help='''Log debug-level output.''')
 
     # Add plugins
     subparsers = parser.add_subparsers(title="Plugins", description="The following plugins are available for use:", metavar='PLUGIN', help='plugin to use')
     for plugin in plugins:
-        try:
-            plugin.register_args(subparsers)
-        except AttributeError:
-            # Don't try to register arguments for subclasses that don't care.
-            pass
+        #try:
+        plugin.register_args(subparsers)
+        #except AttributeError:
+        #    # Don't try to register arguments for subclasses that don't care.
+        #    pass
 
-    pathspider.util.dnsresolv.register_args(subparsers)
+    for util in utils:
+        util.register_args(subparsers)
 
     if len(sys.argv) == 1:
         parser.print_help()
