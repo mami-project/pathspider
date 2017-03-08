ed5b6565f2383cd6507faa283f42b18dd8295aa5
Iain R. Learmonth <irl@fsfe.org>
2017-03-08 22:59:13 +0000

    job_feeder: Allows jobs that don't have dip fields (Closes: #137)

--- pathspider/standalone.py
+++ pathspider/standalone.py
@@ -21,17 +21,14 @@ def job_feeder(inputfile, spider):
                     if 'ip' in job.keys():
                         job['dip'] = job.pop('ip')
                         logger.warning("Job specified 'ip' not 'dip'. This is deprecated and will fail in future PATHspider releases.")
-                    else:
-                        logger.warning("Skipping job due to lack of a target. 'dip' key not present.")
-                        continue
-                if job['dip'] in seen_targets:
-                    logger.warning("This target has already had a job submitted, skipping.")
-                    continue
-                seen_targets.add(job['dip'])
                 if 'dp' not in job.keys():
                     if 'port' in job.keys():
                         job['dp'] = job.pop('port')
                         logger.warning("Job specified 'port' not 'dp'. This is deprecated and will fail in future PATHspider releases.")
+                if job['dip'] in seen_targets:
+                    logger.warning("This target has already had a job submitted, skipping.")
+                    continue
+                seen_targets.add(job['dip'])
                 spider.add_job(job)
             except ValueError:
                 logger.warning("Unable to decode JSON for a job, skipping...")
