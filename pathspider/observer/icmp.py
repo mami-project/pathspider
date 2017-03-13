8ea0bbb994452d2d073cfba172354682f5a844b9
Iain R. Learmonth <irl@fsfe.org>
2017-03-13 21:12:32 +0000

    observer/icmp: Lint

--- pathspider/observer/icmp.py
+++ pathspider/observer/icmp.py
@@ -1,11 +1,11 @@
 
 ICMP_UNREACHABLE = 3
 
-def icmp_setup(rec, ip):
+def icmp_setup(rec, _):
     rec['icmp_unreachable'] = False
     return True
 
-def icmp_unreachable(rec, ip, q, rev):
+def icmp_unreachable(rec, ip, _, rev):
     if rev and ip.icmp.type == ICMP_UNREACHABLE:
         rec['icmp_unreachable'] = True
     return not rec['icmp_unreachable']
