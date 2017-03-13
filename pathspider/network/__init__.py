6311d6ade332ef02e46fef8e3bca6bfd68027680
Iain R. Learmonth <irl@fsfe.org>
2017-03-13 21:51:22 +0000

    network: Do not redefine int

--- pathspider/network/__init__.py
+++ pathspider/network/__init__.py
@@ -1,23 +1,23 @@
 
 from pyroute2 import IPDB # pylint: disable=no-name-in-module
 
-def interface_up(int):
+def interface_up(ifname):
     with IPDB() as ipdb:
-        for interface in set(ipdb.interfaces.values()):
-            if interface.ifname == int and interface.operstate == 'UP':
+        for i in set(ipdb.interfaces.values()):
+            if i.ifname == ifname and interface.operstate == 'UP':
                 return True
         return False
     
-def ipv4_address(int):
+def ipv4_address(ifname):
     with IPDB() as ipdb:
-        addrset = [x[0] for x in ipdb.interfaces[int].ipaddr if '.' in x[0]]
+        addrset = [x[0] for x in ipdb.interfaces[ifname].ipaddr if '.' in x[0]]
         if len(addrset) > 0:
             # Should return the first IPv4 address of the interface...if there are any
             return addrset[0]
 
-def ipv6_address(int):
+def ipv6_address(ifname):
     with IPDB() as ipdb:
-        addrset = [x[0] for x in ipdb.interfaces[int].ipaddr if ':' in x[0] and not x[0].startswith('fe')]
+        addrset = [x[0] for x in ipdb.interfaces[ifname].ipaddr if ':' in x[0] and not x[0].startswith('fe')]
         if len(addrset) > 0:
             # Should return the first IPv6 address of the interface...if there are any
             return addrset[0]
