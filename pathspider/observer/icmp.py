bbff1393c874fb3c4f865e4323fcee8cdf73df36
Iain R. Learmonth <irl@fsfe.org>
2017-03-15 21:22:33 +0000

    observer/icmp: Ignore unused argument

--- pathspider/observer/icmp.py
+++ pathspider/observer/icmp.py
@@ -12,7 +12,7 @@ class ICMPChain(Chain):
     def icmp4(self, rec, ip, q, rev):
         return self._icmp(rec, ip, q, rev)
  
-    def _icmp(self, rec, ip, q, rev):
+    def _icmp(self, rec, ip, q, rev): # pylint: disable=W0613
         if rev and ip.icmp.type == ICMP_UNREACHABLE:
             rec['icmp_unreachable'] = True
         return not rec['icmp_unreachable']
