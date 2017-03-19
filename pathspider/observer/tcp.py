5a6b744a6c441c670c3ce28765ac14aab336534e
Iain R. Learmonth <irl@fsfe.org>
2017-03-19 18:05:18 +0000

    observer/tcp: lint

--- pathspider/observer/tcp.py
+++ pathspider/observer/tcp.py
@@ -71,28 +71,27 @@ class TCPChain(Chain):
     def new_flow(self, rec, ip): # pylint: disable=W0613
         rec['tcp_synflags_fwd'] = None
         rec['tcp_synflags_rev'] = None
-    
+
         rec['tcp_fin_fwd'] = False
         rec['tcp_fin_rev'] = False
         rec['tcp_rst_fwd'] = False
         rec['tcp_rst_rev'] = False
-    
+
         rec['tcp_connected'] = False
-    
+
         return True
-    
+
     def tcp(self, rec, tcp, rev):
         if tcp.syn_flag:
             rec['tcp_synflags_rev' if rev else 'tcp_synflags_fwd'] = tcp.flags
-    
-        # TODO: This test could perhaps be improved upon.
+
         # This test is intended to catch the completion of the 3WHS.
         if (not rec['tcp_connected'] and rev == 0 and
-                rec['tcp_synflags_fwd'] is not None and
-                rec['tcp_synflags_rev'] is not None and
+                rec['tcp_synflags_fwd'] & TCP_SYN == TCP_SYN and
+                rec['tcp_synflags_rev'] & TCP_SA == TCP_SA and
                 tcp.ack_flag):
             rec['tcp_connected'] = True
-    
+
         if tcp.fin_flag and rev:
             rec['tcp_fin_fwd'] = True
         if tcp.fin_flag and not rev:
@@ -101,6 +100,6 @@ class TCPChain(Chain):
             rec['tcp_rst_rev'] = True
         if tcp.rst_flag and not rev:
             rec['tcp_rst_fwd'] = True
-    
+
         return not ((rec['tcp_fin_fwd'] and rec['tcp_fin_rev']) or
                     rec['tcp_rst_fwd'] or rec['tcp_rst_rev'])
