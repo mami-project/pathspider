0aea6b9b3c0f5896aa0692d33016bc9c8a1b30ad
Iain R. Learmonth <irl@fsfe.org>
2017-03-19 18:02:57 +0000

    observer/base: lint

--- pathspider/observer/base.py
+++ pathspider/observer/base.py
@@ -17,17 +17,17 @@ class BasicChain(Chain):
         """
         New flow function that sets up basic flow information
         """
-    
+
         # Extract addresses and ports
         (rec['sip'], rec['dip'], rec['proto']) = (str(ip.src_prefix), str(ip.dst_prefix), ip.proto)
         (rec['sp'], rec['dp']) = self._extract_ports(ip)
-    
+
         # Initialize counters
         rec['pkt_fwd'] = 0
         rec['pkt_rev'] = 0
         rec['oct_fwd'] = 0
         rec['oct_rev'] = 0
-    
+
         # we want to keep this flow
         return True
 
@@ -36,17 +36,17 @@ class BasicChain(Chain):
 
     def ip6(self, rec, ip, rev):
         return self._basic_count(rec, ip, rev)
-    
+
     def _basic_count(self, rec, ip, rev):
         """
         Packet function that counts packets and octets per flow
         """
-    
+
         if rev:
             rec["pkt_rev"] += 1
             rec["oct_rev"] += ip.size
         else:
             rec["pkt_fwd"] += 1
             rec["oct_fwd"] += ip.size
-    
+
         return True
