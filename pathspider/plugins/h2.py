d0e633ca3c61f4142cbdbca79401412fed39938e
Iain R. Learmonth <irl@fsfe.org>
2017-03-21 12:26:50 +0000

    Allows the H2 plugin to use the Tor HTTP connection mode

--- pathspider/plugins/h2.py
+++ pathspider/plugins/h2.py
@@ -1,5 +1,6 @@
 
 import pycurl
+import stem.control
 
 import pathspider.base
 from pathspider.base import PluggableSpider
@@ -7,6 +8,7 @@ from pathspider.base import CONN_OK
 from pathspider.desync import DesynchronizedSpider
 from pathspider.helpers.http import connect_http
 from pathspider.helpers.http import connect_https
+from pathspider.helpers.tor_http import connect_tor_http
 from pathspider.chains.basic import BasicChain
 from pathspider.chains.tcp import TCPChain
 
@@ -16,25 +18,27 @@ class H2(DesynchronizedSpider, PluggableSpider):
     description = "HTTP/2"
     version = pathspider.base.__version__
     chains = [BasicChain, TCPChain]
-    connect_supported = ["http", "https"]
+    connect_supported = ["http", "https", "torhttp"]
 
     def conn_no_h2(self, job, config):  # pylint: disable=unused-argument
         if self.args.connect == "http":
             return connect_http(self.source, job, self.args.timeout)
         if self.args.connect == "https":
             return connect_http(self.source, job, self.args.timeout)
+        if self.args.connect == "torhttp":
+            return connect_tor_http(self.controller, self.args.tor_path, job, self.args.timeout)
         else:
             raise RuntimeError("Unknown connection mode specified")
 
     def conn_h2(self, job, config): # pylint: disable=unused-argument
+        curlopts = {pycurl.HTTP_VERSION: pycurl.CURL_HTTP_VERSION_2_0}
+        curlinfos = {pycurl.INFO_HTTP_VERSION}
         if self.args.connect == "http":
-            curlopts = {pycurl.HTTP_VERSION: pycurl.CURL_HTTP_VERSION_2_0}
-            curlinfos = {pycurl.INFO_HTTP_VERSION}
             return connect_http(self.source, job, self.args.timeout, curlopts, curlinfos)
         if self.args.connect == "https":
-            curlopts = {pycurl.HTTP_VERSION: pycurl.CURL_HTTP_VERSION_2_0}
-            curlinfos = {pycurl.INFO_HTTP_VERSION}
             return connect_https(self.source, job, self.args.timeout, curlopts, curlinfos)
+        if self.args.connect == "torhttp":
+            return connect_tor_http(self.controller, self.args.tor_path, job, self.args.timeout, curlopts, curlinfos)
         else:
             raise RuntimeError("Unknown connection mode specified")
 
