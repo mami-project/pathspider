0dd9a9b73ba2b62f6b2f021af7fa0eb2b2b296db
Iain R. Learmonth <irl@fsfe.org>
2017-04-18 16:36:41 +0100

    TODO basic chain placeholder

---
+++ pathspider/tests/test_chain_basic.py
@@ -0,0 +1,12 @@
+
+# TODO: Need some basic chain tests
+# 1. Packet counts
+# 2. Octet counts
+# 3. Port numbers and addresses for IPv4/TCP
+# 4. Port numbers and addresses for IPv4/UDP
+# 5. Port numbers and addresses for IPv6/TCP
+# 6. Port numbers and addresses for IPv6/UDP
+# 7. Port numbers and addresses for IPv4/SCTP
+# 8. Port numbers and addresses for IPv4/UDP-Lite
+# 9. Port numbers and addresses for IPv4/DCCP
+
