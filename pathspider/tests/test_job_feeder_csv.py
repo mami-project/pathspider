f4401f8ed104ac4f9454c08afca062d0c5450fac
Ana C. Custura <ana@netstat.org.uk>
2017-04-22 23:39:59 +0100

    Modifies the job feeder to accept older style csv files (Closes: #164)

--- pathspider/tests/test_job_feeder_csv.py
+++ pathspider/tests/test_job_feeder_csv.py
@@ -1,7 +1,7 @@
 
 import pkg_resources
 
-from pathspider.cmd.measure import job_feeder_csv
+from pathspider.cmd.measure import job_feeder_csv as job_feeder
 
 class FakeSpider:
     def __init__(self):
@@ -17,40 +17,30 @@ class FakeSpider:
 def test_job_feeder_csv_webtest():
     spider = FakeSpider()
 
-    expected_jobs = [['160.85.31.173', 80, 'mami-project.eu', '1'],
-                     ['139.133.210.32', 80, 'erg.abdn.ac.uk', '2'],
-                     ['2001:630:241:210:569f:35ff:fe0a:116a', 80, 'erg.abdn.ac.uk', '3'],
-                     ['129.132.52.158', 80, 'ecn.ethz.ch', '4'],
-                     ['2001:67c:10ec:36c2::61', 80, 'ecn.ethz.ch', '5'],
-                     ['139.133.1.4', 80, 'abdn.ac.uk', '6']]
+    expected_jobs = [{'dip': '160.85.31.173', 'dp': '80', 'domain': 'mami-project.eu', 'rank': '1'},
+                     {'dip': '139.133.210.32', 'dp': '80', 'domain': 'erg.abdn.ac.uk', 'rank': '2'},
+                     {'dip': '2001:630:241:210:569f:35ff:fe0a:116a', 'dp': '80', 'domain': 'erg.abdn.ac.uk',  'rank': '3'},
+                     {'dip': '129.132.52.158', 'dp': '80', 'domain': 'ecn.ethz.ch', 'rank': '4'},
+                     {'dip': '2001:67c:10ec:36c2::61', 'dp': '80', 'domain': 'ecn.ethz.ch', 'rank': '5'},
+                     {'dip': '139.133.1.4', 'dp': '80', 'domain': 'abdn.ac.uk', 'rank': '6'}]
 
     job_feeder(pkg_resources.resource_filename("pathspider", "tests/data/webtest.csv"), spider)
     assert spider.was_shutdown
+    print (spider.jobs)
+    print (expected_jobs)
     assert spider.jobs == expected_jobs
 
 def test_job_feeder_csv_webtest_newline():
     spider = FakeSpider()
 
-    expected_jobs = [['160.85.31.173', 80, 'mami-project.eu', '1'],
-                     ['139.133.210.32', 80, 'erg.abdn.ac.uk', '2'],
-                     ['2001:630:241:210:569f:35ff:fe0a:116a', 80, 'erg.abdn.ac.uk', '3'],
-                     ['129.132.52.158', 80, 'ecn.ethz.ch', '4'],
-                     ['2001:67c:10ec:36c2::61', 80, 'ecn.ethz.ch', '5'],
-                     ['139.133.1.4', 80, 'abdn.ac.uk', '6']]
+    expected_jobs = [{'dip': '160.85.31.173', 'dp': '80', 'domain': 'mami-project.eu', 'rank': '1'},
+                     {'dip': '139.133.210.32', 'dp': '80', 'domain': 'erg.abdn.ac.uk', 'rank': '2'},
+                     {'dip': '2001:630:241:210:569f:35ff:fe0a:116a', 'dp': '80', 'domain': 'erg.abdn.ac.uk',  'rank': '3'},
+                     {'dip': '129.132.52.158', 'dp': '80', 'domain': 'ecn.ethz.ch', 'rank': '4'},
+                     {'dip': '2001:67c:10ec:36c2::61', 'dp': '80', 'domain': 'ecn.ethz.ch', 'rank': '5'},
+                     {'dip': '139.133.1.4', 'dp': '80', 'domain': 'abdn.ac.uk', 'rank': '6'}]
 
     job_feeder(pkg_resources.resource_filename("pathspider", "tests/data/webtest_newline.csv"), spider)
     assert spider.was_shutdown
     assert spider.jobs == expected_jobs
 
-def test_job_feeder_csv_webtest_badport():
-    spider = FakeSpider()
-
-    expected_jobs = [['160.85.31.173', 80, 'mami-project.eu', '1'],
-                     ['139.133.210.32', 80, 'erg.abdn.ac.uk', '2'],
-                     ['2001:630:241:210:569f:35ff:fe0a:116a', 80, 'erg.abdn.ac.uk', '3'],
-                     ['2001:67c:10ec:36c2::61', 80, 'ecn.ethz.ch', '5'],
-                     ['139.133.1.4', 80, 'abdn.ac.uk', '6']]
-
-    job_feeder(pkg_resources.resource_filename("pathspider", "tests/data/webtest_badport.csv"), spider)
-    assert spider.was_shutdown
-    assert spider.jobs == expected_jobs
