a3a3299ecd5e537f86700b99ee7bd0b3332be1e8
Michael <michiwalter@bluewin.ch>
2017-07-03 16:08:49 +0200

    First version of the traceroute command

---
+++ pathspider/cmd/traceroute.py
@@ -0,0 +1,116 @@
+
+import argparse
+import logging
+import json
+import queue
+import signal
+import sys
+import threading
+import time
+
+from straight.plugin import load
+
+from pathspider.base import SHUTDOWN_SENTINEL
+from pathspider.base import QUEUE_SIZE
+
+from pathspider.chains.base import Chain
+
+from pathspider.observer import Observer
+
+from pathspider.network import interface_up
+
+from pathspider.traceroute_send import send_pkts
+import multiprocessing as mp
+
+chains = load("pathspider.chains", subclasses=Chain)
+
+def run_traceroute(args):
+    logger = logging.getLogger("pathspider")
+
+
+    if not interface_up(args.interface):
+        logger.error("The chosen interface is not up! Cannot continue.")
+        logger.error("Try --help for more information.")
+        sys.exit(1)
+
+    logger.info("Creating observer...")
+
+    chosen_chains = []
+    
+    """geht besser oder???"""    
+    for abc in chains:
+        if "traceroutechain" == abc.__name__.lower():
+            chosen_chains.append(abc)
+
+
+    """Setting up observer"""
+    observer_shutdown_queue = queue.Queue(QUEUE_SIZE)
+    flowqueue = queue.Queue(QUEUE_SIZE)
+    observer = Observer("int:" + args.interface, chosen_chains)
+
+    logger.info("starting observer...")
+    threading.Thread(target=observer.run_flow_enqueuer, args=(flowqueue,observer_shutdown_queue)).start()
+    
+    
+    """Setting up sender"""
+    timequeue = queue.Queue(QUEUE_SIZE) #reicht diese queue size????
+    mp.Process(target=send_pkts,args=(args.hops,args.IP,args.flows,timequeue)).start()
+
+
+    logger.info("opening output file " + args.output)
+    with open(args.output, 'w') as outputfile:
+        logger.info("registering interrupt...")
+        def signal_handler(signal, frame):
+            observer_shutdown_queue.put(True)
+        signal.signal(signal.SIGINT, signal_handler)
+        while True:
+                    
+            result = flowqueue.get()
+                     
+            if result == SHUTDOWN_SENTINEL:
+                logger.info("output complete")
+                break
+            """Only get the flows with the ttl_exceeded message and add timestamp"""
+            result = filter(result, timequeue)
+            
+            if result != []:
+                outputfile.write(json.dumps(result) + "\n")
+                logger.debug("wrote a result")
+
+def filter(res,timequeue):
+    
+    for entry in res:
+        if entry == 'ttl_exceeded' and res[entry] == False:
+            return []
+        """ADD shutdown senitnel und auch noch etwas anderes? zb zwischen flosws... queue grösse usw"""
+        #abc = timequeue.get()
+        #rtt = time.clock()-abc
+        #print(rtt)
+        #else:
+         #   for i in range
+    return res
+
+def register_args(subparsers):
+    class SubcommandHelpFormatter(argparse.RawDescriptionHelpFormatter):
+        def _format_action(self, action):
+            parts = super()._format_action(action)
+            if action.nargs == argparse.PARSER:
+                parts = "\n".join([line for line in parts.split("\n")[1:]])
+                parts += "\n\nSpider safely!"
+            return parts
+
+    parser = subparsers.add_parser(name='traceroute',help="Perform a traceroute",
+                                   formatter_class=SubcommandHelpFormatter)
+    parser.add_argument('IP', type = str, help="IP or URL for which traceroute should be performed")
+    parser.add_argument('hops', type = int, help="Number of hops to destination IP")
+    parser.add_argument('-i', '--interface', default="eth0",
+                        help="The interface to use for the observer. (Default: eth0)")
+    parser.add_argument('-f','--flows', type = int, default = 1, 
+                        help="Number of times the traceroute should be conducted with different flows. (Default: 1)")
+    parser.add_argument('--output', default='/dev/stdout', metavar='OUTPUTFILE',
+                        help=("The file to output results data to. "
+                              "Defaults to standard output."))
+    
+
+    # Set the command entry point
+    parser.set_defaults(cmd=run_traceroute)
