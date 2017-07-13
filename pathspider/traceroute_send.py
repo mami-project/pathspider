6451055eb58b6175c68391ac7f4144c57db4a85f
Michael <michiwalter@bluewin.ch>
2017-07-13 15:57:44 +0200

    new version with ecn

--- pathspider/traceroute_send.py
+++ pathspider/traceroute_send.py
@@ -1,34 +1,43 @@
 from scapy.all import *
 import time
+import queue
+import logging
 from pathspider.base import QUEUE_SIZE
+from pathspider.base import SHUTDOWN_SENTINEL
 
 
 INITIAL_PORT = 10000
 INITIAL_SEQ = 10000
+FLOW_SENTINEL = "Flow-Sentinel"
 
-def send_pkts(ttl_input,dip,src,outqueue):
+def send_pkts(hops,dip,src):
     
     """Send TCP packet with increasing TTL for every hop to destination"""
+    
+    logger = logging.getLogger("sender")
+
     
     hops_add = 3        # buffer for additional hops
     
     time.sleep(2)   #wait short time to guarantee that observer finished setting up
     
     # check for possible initial max hops
-    if ttl_input > 128:
-        hops = 256 - ttl_input + hops_add
-    elif ttl_input > 64:
-        hops = 129 - ttl_input + hops_add
-    elif ttl_input > 32:
-        hops = 65 - ttl_input + hops_add
-    else:
-        hops = 33 - ttl_input + hops_add
+    #if ttl_input > 128:
+     #   hops = 256 - ttl_input + hops_add
+    #elif ttl_input > 64:
+     #   hops = 129 - ttl_input + hops_add
+    #elif ttl_input > 32:
+     #   hops = 65 - ttl_input + hops_add
+    #else:
+     #   hops = 33 - ttl_input + hops_add
   
     for j in range(src):    #repeating with src different flows
         for i in range(hops):
-            send(IP(ttl=i,dst = dip)/TCP(seq=(INITIAL_SEQ-1+i),sport = (INITIAL_PORT-1+j)))
-            outqueue.put(time.clock())
-        #send(IP(ttl=i,dst = dip)/TCP(sport = (INITIAL_PORT-1+i)))
+            send(IP(ttl=(i+1),dst = dip, tos = 0x03 )/TCP(seq=(INITIAL_SEQ+i),sport = (INITIAL_PORT+j), dataofs = 6, options = [('MSS', (536))]), verbose=0)
+            time.sleep(0.1)    
+        time.sleep(0.25)
+        logger.info(("Sending flow number %u finished"), (j+1))
+
     
     return
 
@@ -40,7 +49,3 @@ def send_pkt(ttl_input,dip):
     
     
     return
-
-#send_pkt(2,"172.217.18.99")
-
-#send_pkts(45,"172.217.18.99")   #54 is TTL that came back from destination
