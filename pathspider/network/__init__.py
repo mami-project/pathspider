7c4b02369cbc67752362dcebb8039df1ac29304f
Iain R. Learmonth <irl@fsfe.org>
2017-08-31 14:23:13 +0100

    Adds functions in the network module for RIPEstat data lookups
    Provides the ability to lookup the public v4 and v6 addresses when
    using Internet as seen by RIPEstat and the ability to lookup the
    ASN that announces it.

--- pathspider/network/__init__.py
+++ pathspider/network/__init__.py
@@ -1,4 +1,8 @@
 
+from io import BytesIO
+import json
+
+import pycurl
 from pyroute2 import IPDB # pylint: disable=no-name-in-module
 
 def interface_up(ifname):
@@ -22,3 +26,51 @@ def ipv6_address(ifname):
         if len(addrset) > 0:
             # Should return the first IPv6 address of the interface...if there are any
             return addrset[0]
+
+def ipv4_address_public(ifname):
+    c = pycurl.Curl()
+    body = BytesIO()
+    c.setopt(c.URL, "https://stat.ripe.net/data/whats-my-ip/data.json")
+    c.setopt(c.INTERFACE, ifname)
+    c.setopt(c.WRITEDATA, body)
+    c.setopt(c.IPRESOLVE, c.IPRESOLVE_V4)
+    c.perform()
+    return json.loads((body.getvalue()).decode('utf-8'))['data']['ip']
+
+def ipv6_address_public(ifname):
+    c = pycurl.Curl()
+    body = BytesIO()
+    c.setopt(c.URL, "https://stat.ripe.net/data/whats-my-ip/data.json")
+    c.setopt(c.INTERFACE, ifname)
+    c.setopt(c.WRITEDATA, body)
+    c.setopt(c.IPRESOLVE, c.IPRESOLVE_V6)
+    c.perform()
+    return json.loads((body.getvalue()).decode('utf-8'))['data']['ip']
+
+def ipv4_asn(ifname):
+    c = pycurl.Curl()
+    body = BytesIO()
+    c.setopt(c.URL, "https://stat.ripe.net/data/prefix-overview/data.json?resource={}"
+             .format(ipv4_address_public(ifname)))
+    c.setopt(c.INTERFACE, ifname)
+    c.setopt(c.WRITEDATA, body)
+    c.perform()
+    asns = json.loads((body.getvalue()).decode('utf-8'))['data']['asns']
+    if len(asns) == 1:
+        return asns[0]['asn']
+    else:
+        return None
+
+def ipv6_asn(ifname):
+    c = pycurl.Curl()
+    body = BytesIO()
+    c.setopt(c.URL, "https://stat.ripe.net/data/prefix-overview/data.json?resource={}"
+             .format(ipv6_address_public(ifname)))
+    c.setopt(c.INTERFACE, ifname)
+    c.setopt(c.WRITEDATA, body)
+    c.perform()
+    asns = json.loads((body.getvalue()).decode('utf-8'))['data']['asns']
+    if len(asns) == 1:
+        return asns[0]['asn']
+    else:
+        return None
