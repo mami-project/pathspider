3beac663dc77172c33830d32effd4d33aeb48458
Iain R. Learmonth <irl@fsfe.org>
2017-10-09 17:52:30 +0100

    Add tests to ensure that duplicates are filtered by job feeder (JSONND and CSV) (Closes: #176)

--- pathspider/tests/test_job_feeder_csv.py
+++ pathspider/tests/test_job_feeder_csv.py
@@ -44,3 +44,18 @@ def test_job_feeder_csv_webtest_newline():
     assert spider.was_shutdown
     assert spider.jobs == expected_jobs
 
+def test_job_feeder_csv_webtest_duplicates():
+    spider = FakeSpider()
+
+    expected_jobs = [{'dip': '160.85.31.173', 'dp': '80', 'domain': 'mami-project.eu', 'rank': '1'},
+                     {'dip': '139.133.210.32', 'dp': '80', 'domain': 'erg.abdn.ac.uk', 'rank': '2'},
+                     {'dip': '2001:630:241:210:569f:35ff:fe0a:116a', 'dp': '80', 'domain': 'erg.abdn.ac.uk',  'rank': '3'},
+                     {'dip': '129.132.52.158', 'dp': '80', 'domain': 'ecn.ethz.ch', 'rank': '4'},
+                     {'dip': '2001:67c:10ec:36c2::61', 'dp': '80', 'domain': 'ecn.ethz.ch', 'rank': '5'},
+                     {'dip': '139.133.1.4', 'dp': '80', 'domain': 'abdn.ac.uk', 'rank': '6'}]
+
+    job_feeder(pkg_resources.resource_filename("pathspider", "tests/data/webtest_duplicates.csv"), spider)
+    assert spider.was_shutdown
+    print (spider.jobs)
+    print (expected_jobs)
+    assert spider.jobs == expected_jobs
