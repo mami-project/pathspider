9c37015a2bfd537df18ad144891827e244edf397
Michael <michiwalter@bluewin.ch>
2017-10-23 17:56:03 +0200

    fixed ipv6 attribute error problem

--- pathspider/chains/traceroute.py
+++ pathspider/chains/traceroute.py
@@ -70,13 +70,24 @@ class tracerouteChain(Chain):
         return True
             
     def icmp4(self, rec, ip, q, rev):
-        if rev and ip.icmp.type == ICMP4_TTLEXCEEDED:         
-            self.trace(rec, ip, ip.icmp)
+        if rev and ip.icmp.type == ICMP4_TTLEXCEEDED:
+            try:
+                tcp_seq = ip.icmp.payload.tcp.seq_nbr  
+            except AttributeError:
+                print("IPv4 Sequence error")
+            else:      
+                self.trace(rec, ip, ip.icmp, tcp_seq)
         return True
     
     def icmp6(self, rec, ip6, q, rev):    
         if rev and ip6.icmp6.type == ICMP6_TTLEXCEEDED:
-            self.trace(rec, ip6, ip6.icmp6)
+            try:
+                tcp_payload = ip6.icmp6.payload.payload         
+                tcp_seq = (tcp_payload[4]<<24)+(tcp_payload[5]<<16)+(tcp_payload[6]<<8)+tcp_payload[7]
+            except AttributeError:
+                print("IPv6 Sequence error")
+            else:
+                self.trace(rec, ip6, ip6.icmp6, tcp_seq)
         return True
         
     def dest_seq(self, rec, ip):
@@ -112,36 +123,33 @@ class tracerouteChain(Chain):
                         plugin_out = getattr(ch, "box_info")(ip)
                         rec['Destination']['conditions'] = plugin_out
     
-    def trace(self, rec, ip, icmp):
+    def trace(self, rec, ip, icmp, tcp_seq):
         
         """Packets should be merged by tracemerger"""
         rec['trace'] = True
         
         """Identification of hop number via sequence number""" #check if it works now... problem if icmp has no tcp??
-        try:
-            hopnumber = str(icmp.payload.tcp.seq_nbr - (INITIAL_SEQ-1))
-        except AttributeError:
-            print("Sequence Attribute Error!")
-        else:
-            """IP of middlebox"""
-            box_ip = str(ip.src_prefix)
-              
-            """Packet arrival time for calculation of rtt in merger"""
-            time = ip.seconds
-                       
-            """length of payload that comes back to identify RFC1812-compliant routers"""
-            pp = icmp.payload.payload
-            payload_len = len(pp)
-             
-            """payload data of returning packet for bitwise comparison in merger""" 
-            data = icmp.payload.data
-            
-            rec[hopnumber] = {'from': box_ip, 'rtt': time, 'size': payload_len, 'data': data}
-            
-            """Additional 'conditions' info of additional plugin chains"""      
-            if len(chosen_chains) > 0:
-                for c in chosen_chains: 
-                    ch = c()
-                    if hasattr(ch, 'box_info'):
-                        plugin_out = getattr(ch, "box_info")(ip)
-                        rec[hopnumber]['conditions'] = plugin_out             
+        hopnumber = str(tcp_seq - (INITIAL_SEQ-1))
+        
+        """IP of middlebox"""
+        box_ip = str(ip.src_prefix)
+          
+        """Packet arrival time for calculation of rtt in merger"""
+        time = ip.seconds
+                   
+        """length of payload that comes back to identify RFC1812-compliant routers"""
+        pp = icmp.payload.payload
+        payload_len = len(pp)
+         
+        """payload data of returning packet for bitwise comparison in merger""" 
+        data = icmp.payload.data
+        
+        rec[hopnumber] = {'from': box_ip, 'rtt': time, 'size': payload_len, 'data': data}
+        
+        """Additional 'conditions' info of additional plugin chains"""      
+        if len(chosen_chains) > 0:
+            for c in chosen_chains: 
+                ch = c()
+                if hasattr(ch, 'box_info'):
+                    plugin_out = getattr(ch, "box_info")(ip)
+                    rec[hopnumber]['conditions'] = plugin_out             
