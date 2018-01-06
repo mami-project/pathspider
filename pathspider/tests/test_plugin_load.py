3a82ae60079fa7c18595a1aa73ed368cb9ccbdb9
Iain R. Learmonth <irl@fsfe.org>
2018-01-06 17:37:37 +0000

    Removes TFO and UDP Options plugins

--- pathspider/tests/test_plugin_load.py
+++ pathspider/tests/test_plugin_load.py
@@ -11,7 +11,7 @@ def test_plugin_load():
     except ImportError:
         raise nose.SkipTest
 
-    expected_names = set(['TFO', 'ECN', 'DSCP', 'UDPZero', 'UDPOpts', 'DNSResolv', 'H2', 'EvilBit'])
+    expected_names = set(['ECN', 'DSCP', 'UDPZero', 'DNSResolv', 'H2', 'EvilBit'])
     names = set()
 
     for plugin in pathspider.cmd.measure.plugins:
