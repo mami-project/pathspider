115c7eae84d2cdc095d8ae0845368b7a7fdf8e0c
Iain R. Learmonth <irl@fsfe.org>
2018-01-11 00:57:57 +0000

    base: Demote warning about missed flows to debug

--- pathspider/base.py
+++ pathspider/base.py
@@ -373,7 +373,7 @@ class Spider:
                     job['missed_flows'] = job['missed_flows'] + 1
             job['conditions'] = self.combine_flows(flows)
             if "pathspider.not_observed" in job['conditions']:
-                self.__logger.warning("At least one flow was not observed and so conditions could not be fully generated (if at all)")
+                self.__logger.debug("At least one flow was not observed and so conditions could not be fully generated (if at all)")
             if job['missed_flows'] > 0:
                 job['conditions'].append("pathspider.missed_flows:" + str(job['missed_flows']))
             if job['conditions'] is None:
