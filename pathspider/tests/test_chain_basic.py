9ca1b553257a30174bf489127bfb2e507022c6d5
Ana C. Custura <ana@netstat.org.uk>
2018-01-16 05:36:10 +0000

    Adds pcap files for testing the basic udp chain Adds basic chain tests for UDP

--- pathspider/tests/test_chain_basic.py
+++ pathspider/tests/test_chain_basic.py
@@ -52,3 +52,49 @@ class TestBasicChain(ChainTestCase):
 
         for key in expected_basic:
             assert flows[0][key] == expected_basic[key]
+
+    def test_chain_basic_ipv4_udp(self):
+        test_trace = "basic_ipv4_udp.pcap"
+        self.create_observer(test_trace, [BasicChain])
+
+        expected_basic = {
+            'oct_fwd': 56,
+            'dp': 53,
+            'pkt_rev': 1,
+            'pkt_fwd': 1,
+            'proto': 17,
+            'oct_rev': 267,
+            'sip': '172.22.152.138',
+            'sp': 57606,
+            'dip': '8.8.8.8'
+        }
+
+        flows = self.run_observer()
+        print(flows)
+        assert len(flows) == 1
+
+        for key in expected_basic:
+            assert flows[0][key] == expected_basic[key]
+
+    def test_chain_basic_ipv6_udp(self):
+        test_trace = "basic_ipv6_udp.pcap"
+        self.create_observer(test_trace, [BasicChain])
+
+        expected_basic = {
+            'oct_fwd': 76,
+            'dp': 53,
+            'pkt_rev': 1,
+            'pkt_fwd': 1,
+            'proto': 17,
+            'oct_rev': 287,
+            'sip': '2001:470:1d58:1337:4100:e1a1:8dcf:488',
+            'sp': 40672,
+            'dip': '2001:4860:4860::8888',
+        }
+
+        flows = self.run_observer()
+        print(flows)
+        assert len(flows) == 1
+
+        for key in expected_basic:
+            assert flows[0][key] == expected_basic[key]
