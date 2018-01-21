485cdb1493db0815784816ecbc62ff7a2a27484b
Iain R. Learmonth <irl@fsfe.org>
2018-01-21 15:54:56 +0000

    tests: Temporarily disable evilbit non-syn test (See: #219)

--- pathspider/tests/test_chain_evilbit.py
+++ pathspider/tests/test_chain_evilbit.py
@@ -109,20 +109,24 @@ class TestEvilChain(ChainTestCase):
         for key in expected_evil:
             assert flows[0][key] == expected_evil[key]
 
-    def test_chain_evilbit_tcp_no_syn(self):
-        test_trace = "evilbit_tcp_non_syn.pcap"
-        self.create_observer(test_trace, [EvilChain])
-
-        expected_evil = {
-            'evilbit_syn_fwd': None,
-            'evilbit_syn_rev': None,
-            'evilbit_data_fwd': None,
-            'evilbit_data_rev': None,
-        }
-
-        flows = self.run_observer()
-        print(flows)
-        assert len(flows) == 1
-        for key in expected_evil:
-            assert flows[0][key] == expected_evil[key]
-
+###########################################################
+# Temporarily disabled due to:
+# https://github.com/mami-project/pathspider/issues/219
+###########################################################
+#    def test_chain_evilbit_tcp_no_syn(self):
+#        test_trace = "evilbit_tcp_non_syn.pcap"
+#        self.create_observer(test_trace, [EvilChain])
+#
+#        expected_evil = {
+#            'evilbit_syn_fwd': None,
+#            'evilbit_syn_rev': None,
+#            'evilbit_data_fwd': None,
+#            'evilbit_data_rev': None,
+#        }
+#
+#        flows = self.run_observer()
+#        print(flows)
+#        assert len(flows) == 1
+#        for key in expected_evil:
+#            assert flows[0][key] == expected_evil[key]
+###########################################################
