2c77f31e5738ee2182125f35a6862ae1b0d173b5
Ana C. Custura <ana@netstat.org.uk>
2018-07-17 16:28:45 +0100

    Adds tests and traces for TCP Fast Open chain using EXPB

--- pathspider/tests/test_chain_tfo.py
+++ pathspider/tests/test_chain_tfo.py
@@ -5,7 +5,7 @@ from pathspider.chains.tfo import TFOChain
 
 class TestTFOChain(ChainTestCase):
 
-    def test_chain_tfo_cookie_request(self):
+    def test_chain_tfo_cookie_request_expa(self):
         test_trace = "tfo_cookie_request.pcap"
         self.create_observer(test_trace, [TFOChain])
 
@@ -28,7 +28,7 @@ class TestTFOChain(ChainTestCase):
         for key in expected_tfo:
             assert flows[0][key] == expected_tfo[key]
 
-    def test_chain_tfo_data_on_syn(self):
+    def test_chain_tfo_data_on_syn_expa(self):
         test_trace = "tfo_data_on_syn.pcap"
         self.create_observer(test_trace, [TFOChain])
 
@@ -47,6 +47,50 @@ class TestTFOChain(ChainTestCase):
 
         print(flows)
         print(expected_tfo)
+
+    def test_chain_tfo_cookie_request_expb(self):
+        test_trace = "tfo_cookie_request_b.pcap"
+        self.create_observer(test_trace, [TFOChain])
+
+        expected_tfo = {
+            'tfo_ack': 3865413713,
+            'tfo_ackclen': 8,
+            'tfo_ackkind': 255,
+            'tfo_dlen': 0,
+            'tfo_seq': 3865413712,
+            'tfo_synclen': 0,
+            'tfo_synkind': 255,
+        }
+
+        flows = self.run_observer()
+        assert len(flows) == 1
+
+        print(flows)
+        print(expected_tfo)
+
+        for key in expected_tfo:
+            assert flows[0][key] == expected_tfo[key]
+
+    def test_chain_tfo_data_on_syn_expb(self):
+        test_trace = "tfo_data_on_syn_b.pcap"
+        self.create_observer(test_trace, [TFOChain])
+
+        expected_tfo = {
+            'tfo_ack': 3108342141,
+            'tfo_ackclen': 0,
+            'tfo_ackkind': 0,
+            'tfo_dlen': 39,
+            'tfo_seq': 3108342101,
+            'tfo_synclen': 8,
+            'tfo_synkind': 255,
+        }
+
+        flows = self.run_observer()
+        assert len(flows) == 1
+
+        print(flows)
+        print(expected_tfo)
+
 
         for key in expected_tfo:
             assert flows[0][key] == expected_tfo[key]
