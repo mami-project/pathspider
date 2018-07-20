179d864c37df44c8590affb412467c4cecbe3080
nstudach <nstudach@gmail.com>
2018-07-21 01:10:05 +0200

    Update base.py
    removed useless comments

--- pathspider/cmd/base.py
+++ pathspider/cmd/base.py
@@ -8,7 +8,6 @@ import pathspider.cmd.filter
 import pathspider.cmd.measure
 import pathspider.cmd.observe
 import pathspider.cmd.test
-#new cmd
 import pathspider.cmd.upload
 
 cmds = [
@@ -17,7 +16,6 @@ cmds = [
     pathspider.cmd.measure,
     pathspider.cmd.observe,
     pathspider.cmd.test,
-    #add new cmd
     pathspider.cmd.upload
 ]
 
